def28c91a0411033778aac99ef8d1a520a09db97
Alamelu.V <alamelu.gotara@gmail.com>
2025-01-16 13:47:33 +0800

    tech: update the title

--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,7 @@ import QuizApp from './components/QuizApp';
 export default function Home() {
   return (
     <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">3rd Class Test App</h1>
+      <h1 className="text-3xl font-bold mb-4">6th Class Test App</h1>
       <QuizApp />
     </main>
   )
